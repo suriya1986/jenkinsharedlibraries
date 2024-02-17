efd350ba8e9b5f9f7852daafc1e6eca0ae554702
Suriyanarayanan Ramanujam <suriya1986@gmail.com>
2024-02-17 13:46:24 +0530

    getbuildversion

--- vars/buildversion.groovy
+++ vars/buildversion.groovy
@@ -4,10 +4,10 @@ import java.time.temporal.IsoFields;
 import groovy.json.JsonSlurper
 
 def getBuildVersion() {
-     def jsonSlurper = new JsonSlurper()
-    def buildInfo = jsonSlurper.parse(new File('buildInfo.json'))
-    println "buildInfo = $buildInfo"
-    echo "Hello, ${buildInfo.buildVersion}."
+    //  def jsonSlurper = new JsonSlurper()
+    // def buildInfo = jsonSlurper.parse(new File('buildInfo.json'))
+    // println "buildInfo = $buildInfo"
+    // echo "Hello, ${buildInfo.buildVersion}."
     def date = new Date()
     echo LocalDate.now().get(IsoFields.QUARTER_OF_YEAR);
     echo "Hello, ${name}."
