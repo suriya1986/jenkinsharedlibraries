67677eef29494f8812c53bae641066e2afb6db18
Suriyanarayanan Ramanujam <suriya1986@gmail.com>
2024-02-17 14:26:32 +0530

    read file

--- vars/getBuildVersion.groovy
+++ vars/getBuildVersion.groovy
@@ -6,7 +6,7 @@ import groovy.json.JsonSlurper
 def call() {
      def jsonSlurper = new JsonSlurper()
      echo "${env.WORKSPACE}"
-    def buildInfo = jsonSlurper.parse(new File('${env.WORKSPACE}/build/buildInfo.json'))
+    def buildInfo = jsonSlurper.parse(new File("${env.WORKSPACE}/build/buildInfo.json"))
     println "buildInfo = $buildInfo"
     echo "Hello, ${buildInfo.buildVersion}."
     def date = new Date()
