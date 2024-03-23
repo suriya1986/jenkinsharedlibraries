32c6f30ee6a0bac1c622bca067371f4aa6d24d67
Suriyanarayanan Ramanujam <suriya1986@gmail.com>
2024-03-23 09:30:38 +0530

    buildversion changes

--- vars/getBuildVersion.groovy
+++ vars/getBuildVersion.groovy
@@ -4,12 +4,24 @@ import java.time.temporal.IsoFields;
 import groovy.json.JsonSlurper
 
 def call() {
-     def jsonSlurper = new JsonSlurper()
-     echo "${env.WORKSPACE}"
-    def buildInfo = jsonSlurper.parse(new File("${env.WORKSPACE}/build/buildInfo.json"))
-    println "buildInfo = $buildInfo"
-    echo "Hello, ${buildInfo.buildVersion}."
-    def date = new Date()
-    echo LocalDate.now().get(IsoFields.QUARTER_OF_YEAR).toString();
-    echo this.currentBuild.displayName
+    assert GIT_BRANCH != null
+    assert BUILD_NUMBER != null
+ 
+    def gitBranch = GIT_BRANCH.toLowerCase()
+    def buildInfo = readFile buildInfoJsonFilePath
+    def slurper = new JsonSlurper()
+    def buildInfoJson = slurper.parseText(buildInfo)
+    def buildSuffix = (gitBranch=="master"||gitBranch.startsWith("release")) ? "" : "-beta"  
+    def datePart = java.time.LocalDate.now().format(java.time.format.DateTimeFormatter.ofPattern("yyQ.MMdd"))
+    def buildVersion = buildInfoJson.buildVersion + "." + datePart + "." + BUILD_NUMBER + buildSuffix
+    echo buildVersion
+    return buildVersion
+    //  def jsonSlurper = new JsonSlurper()
+    //  echo "${env.WORKSPACE}"
+    // def buildInfo = jsonSlurper.parse(new File("${env.WORKSPACE}/build/buildInfo.json"))
+    // println "buildInfo = $buildInfo"
+    // echo "Hello, ${buildInfo.buildVersion}."
+    // def date = new Date()
+    // echo LocalDate.now().get(IsoFields.QUARTER_OF_YEAR).toString();
+    // echo this.currentBuild.displayName
 }
